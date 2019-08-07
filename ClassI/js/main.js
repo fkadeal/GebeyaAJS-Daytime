3b70f94499285cc668cae1aaa351f629a1ea3baf
DaniDPX <tech@danidpx.com>
2019-08-07 11:16:05 +0300

    mid way

---
+++ ClassI/js/main.js
@@ -0,0 +1,56 @@
+// console.log("This is a test");
+
+
+// var Student = {
+
+//     firstname: "Gebeya",
+//     lastname:"Talent"
+// }
+
+// console.log(Student.firstname);
+
+
+
+// var onSale = true;
+// console.log(typeof(onSale));
+
+
+// var onSale = 12; 
+// var discount = 3;
+
+
+
+
+
+var onSale = true, inventoryLevel = 12, discount = 3;
+
+if (onSale && inventoryLevel > 10) 
+{
+    console.log("We have plenty left");
+}
+
+
+
+
+if (onSale || discount > 0) {
+    console.log("On sale!");
+} else {
+    console.log("Full price");
+}
+
+
+
+
+
+
+
+var rect = {
+    width: 100,
+    height: 50,
+    toString: function () {
+        return "Width: " + this.width + ", height: " + this.height;
+    }
+};
+console.log(rect);
+// console.log("My object rect is: " + rect); 
+console.log("My object rect is: " + rect.toString()); 
