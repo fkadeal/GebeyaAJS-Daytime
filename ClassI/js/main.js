3ac3d2805acb6568c2375b99fdff70c5bc1a6270
DaniDPX <tech@danidpx.com>
2019-08-16 10:29:30 +0300

    Ternary operator example is added

--- ClassI/js/main.js
+++ ClassI/js/main.js
@@ -22,35 +22,202 @@
 
 
 
-var onSale = true, inventoryLevel = 12, discount = 3;
+// var onSale = true, inventoryLevel = 12, discount = 3;
 
-if (onSale && inventoryLevel > 10) 
-{
-    console.log("We have plenty left");
-}
-
-
+// if (onSale && inventoryLevel > 10) 
+// {
+//     console.log("We have plenty left");
+// }
 
 
-if (onSale || discount > 0) {
-    console.log("On sale!");
-} else {
-    console.log("Full price");
-}
 
 
+// if (onSale || discount > 0) {
+//     console.log("On sale!");
+// } else {
+//     console.log("Full price");
+// }
 
 
 
 
 
-var rect = {
-    width: 100,
-    height: 50,
-    toString: function () {
-        return "Width: " + this.width + ", height: " + this.height;
-    }
-};
-console.log(rect);
+
+
+// var rect = {
+//     width: 100,
+//     height: 50,
+//     toString: function () {
+//         return "Width: " + this.width + ", height: " + this.height;
+//     }
+// };
+// console.log(rect);
 // console.log("My object rect is: " + rect); 
-console.log("My object rect is: " + rect.toString()); 
+
+// console.log("My object rect is: " + rect.toString()); 
+
+
+
+
+
+
+
+// var noValue = null;
+
+// console.log(noValue)
+
+
+
+
+
+
+// for (index = 0; index < 10; index++) {
+//      //element = array[index];
+//     document.write(index);
+    
+// }
+
+
+
+
+
+//  CLASS 3
+
+// function Bike(model,color){
+//     this.model = model,
+//     this.color = color,
+//     this.getDetails = function(){
+//       return this.model+' bike is '+this.color;
+//     }
+//  }
+//  var bikeObj1 = new Bike('BMW','BLACK');
+//  var bikeObj2 = new Bike('BMW','WHITE');
+ 
+
+
+//  console.log(bikeObj1);
+//  console.log(bikeObj1.getDetails.__proto__.length);
+
+//  var someVariable = true
+
+//  someVariable ? console.log("It is true"):console.log("It is false")
+
+
+
+
+// Class Bike{
+//     constructor(model,color)
+//     {
+//           this.model = model,
+//             this.color = color,
+
+//     }
+// }
+
+
+//  var bikeObj1 = new Bike('BMW','BLACK');
+//  var bikeObj2 = new Bike('BMW','WHITE');
+ 
+
+//  console.log(bikeObj1);
+//  console.log(bikeObj2);
+
+
+// (function()
+// {
+//     var Hello;
+//     Hello = "IIFE test";
+//     console.log(Hello);
+// })();
+
+
+// function IIFE()
+// {
+//     var Hello;
+//     Hello = "IIFE test";
+//     console.log(Hello);
+// };
+
+// console.log(Hello);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Student(name)
+// {
+
+//     var displayName = function(greeting)
+//     {
+//         console.log(greeting + ", " + name);
+        
+//     }
+
+//     return displayName;
+// }
+
+
+
+
+// var GebeyaStudent1 = Student('Kalkidan');
+// var GebeyaStudent2 = Student('Meron');
+
+
+// console.log(GebeyaStudent1("Hi"));
+// console.log(GebeyaStudent2("Selam"));
+
+
+
+
+
+
+
+
+// var firstValue = 3;
+// var secondValue 
+// document.write(firstValue + secondValue);
+
+
+
+// console.log(
+// secondValue = 6;
+
+// console.log("___________________________");
+
+// var firstValue = 3;
+// var secondValue;
+
+// console.log(firstValue + secondValue);
+
+// console.log("___________________________");
+
+// firstValue = 8;
+// secondValue = 1;
+
+// console.log(firstValue);
+// console.log(secondValue);
+
+
+
+
+
+// (function(name) {
+//     console.log('Hi I am ' + name)
+// }('Abel'))
+
+
+
+
+
+
+
+
+
